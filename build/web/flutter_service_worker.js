19d04dd85d611848d507e304f90f577165732dff
Pedro Reis <41798297+PedroReisCwb@users.noreply.github.com>
2020-05-26 19:14:04 -0300

    Adicionando arquivo web

---
+++ build/web/flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "4c90a67f8169bac3a7fa280dcb3a60c5",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/images/logo.png": "019e38d0edd32fc5496ce16846893e2c",
+"assets/LICENSE": "829acbc245bd56f693d004352b598b74",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "410f138240c74c583a5ba95df5998bc8",
+"/": "410f138240c74c583a5ba95df5998bc8",
+"main.dart.js": "1f6402bd85f970ceecd85cd15a073846",
+"manifest.json": "cc07068057211e855d3bb2ee32905984"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
